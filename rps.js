0c5ddcbde5355e3791f7019411240c5363e92f34
Kishan Chauhan <mtcup2000@yahoo.com>
2024-12-14 13:43:56 -0600

    Pretty much finished, Will try to implement a play again button that will reset the game

--- rps.js
+++ rps.js
@@ -1,5 +1,6 @@
 
-function getHumanChoice() {
+function getHumanChoice() 
+{
     return new Promise((resolve) => {
         // Add event listeners to buttons to capture the user's choice
         rockButton.addEventListener("click", () => {
@@ -15,6 +16,7 @@ function getHumanChoice() {
         }, { once: true });
     });
 }
+
 function getComputerChoice()
 {
     let computerChoice = Math.floor(Math.random() * 3);
@@ -45,71 +47,123 @@ function incrementComputerScore()
     computerScoreElement.textContent = computerScore; // Update the display
 }
 
-function playRound(humanChoice, computerChoice)
+// Function to delay execution
+function delay(ms) 
 {
-    if(humanChoice == "Rock")
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Function to announce "Rock, Paper, Scissors, Shoot!"
+async function announceCycle(userChoice, computerChoice)
+{
+    const announcements = ["Rock", "Paper", "Scissors", "Shoot!", computerChoice];
+    
+    for (let i = 0; i < announcements.length; i++) 
     {
-        if(computerChoice == "Scissors")
+        announcementTextElement.textContent = announcements[i];
+        
+        if(i == 4)
+        {
+            userPickTextElement.textContent = userChoice;
+        }
+
+        await delay(600); // Wait .6 second before showing the next announcement
+    }
+}
+
+function playRound(humanChoice, computerChoice) 
+{
+    if (humanChoice == "Rock") 
+    {
+        if (computerChoice == "Scissors") 
         {
             incrementUserScore();
-            console.log("You win this round, Rock beats Scissors!")
-        }
-        else if(computerChoice == "Paper")
+            setTimeout(() => {
+                announcementTextElement.textContent = "You win this round, Rock beats Scissors!";
+            }, 600); // .6-second delay
+        } 
+        else if (computerChoice == "Paper")
         {
             incrementComputerScore();
-            console.log("Loser, Paper beats Rock!")
-        }
+            setTimeout(() => {
+                announcementTextElement.textContent = "Loser, Paper beats Rock!";
+            }, 600);
+        } 
         else
         {
-            console.log("We both went Rock!")
+            setTimeout(() => {
+                announcementTextElement.textContent = "We both went Rock! Lets try again";
+            }, 600);
+        }
+    } 
+    else if (humanChoice == "Paper") 
+    {
+        if (computerChoice == "Rock") 
+        {
+            incrementUserScore();
+            setTimeout(() => {
+                announcementTextElement.textContent = "You win this round, Paper beats Rock!";
+            }, 600);
+        } 
+        else if (computerChoice == "Scissors") 
+        {
+            incrementComputerScore();
+            setTimeout(() => {
+                announcementTextElement.textContent = "Loser, Scissors beats Paper!";
+            }, 600);
+        } 
+        else 
+        {
+            setTimeout(() => {
+                announcementTextElement.textContent = "We both went Paper! Lets try again";
+            }, 600);
         }
     }
-    else if(humanChoice == "Paper")
+    else if (humanChoice == "Scissors") 
     {
-        if(computerChoice == "Rock")
+        if (computerChoice == "Paper") 
         {
             incrementUserScore();
-            console.log("You win this round, Paper beats Rock!")
-        }
-        else if(computerChoice == "Scissors")
+            setTimeout(() => {
+                announcementTextElement.textContent = "You win this round, Scissors beats Paper!";
+            }, 600);
+        } 
+        else if (computerChoice == "Rock") 
         {
             incrementComputerScore();
-            console.log("Loser, Scissors beats Paper!")
-        }
-        else
+            setTimeout(() => {
+                announcementTextElement.textContent = "Loser, Rock beats Scissors!";
+            }, 600);
+        } 
+        else 
         {
-            console.log("We both went Paper!")
-        }
-    }
-    else if(humanChoice == "Scissors")
-    {
-        if(computerChoice == "Paper")
-        {
-            incrementUserScore();
-            console.log("You win this round, Scissors beats Paper!")
-        }
-        else if(computerChoice == "Rock")
-        {
-            incrementComputerScore();
-            console.log("Loser, Rock beats Scissors!")
-        }
-        else
-        {
-            console.log("We both went Scissors!")
+            setTimeout(() => {
+                announcementTextElement.textContent = "We both went Scissors! Lets try again";
+            }, 600);
         }
     }
 }
 
+
 async function playGame(x)
 {
-    announcementTextElement.textContent = "Choose Your Hand";
+    announcementTextElement.textContent = `Best out of ${x}`;
+    await delay(1000);
 
-    for(let i=0; i<x; i++)
+    while(userScore < x && computerScore < x)
     {
+        announcementTextElement.textContent = "Choose Your Hand";
+        
         let userChoice = await getHumanChoice(); 
         let computerChoice = getComputerChoice();
 
+        await announceCycle(userChoice, computerChoice);
+
         playRound(userChoice, computerChoice);
+
+        // Add a delay after the winner/loser announcement
+        await delay(5000); // Wait 5 seconds before the next round
+        userPickTextElement.textContent = "";
     }
 
     console.log(`User Score: ${userScore}`);
@@ -117,14 +171,18 @@ async function playGame(x)
 
     if(userScore > computerScore)
     {
+        announcementTextElement.textContent = "YOU WIN!";
         console.log("YOU WIN!")
     }
     else if(userScore < computerScore)
     {
+        announcementTextElement.textContent = "YOU LOSE!";
         console.log("YOU LOSE!")
     }
     else
     {
+        // Shouldnt be possible but whatever
+        announcementTextElement.textContent = "TIE!";
         console.log("TIE!")
     }
 }
@@ -135,6 +193,7 @@ const startButton = document.getElementById("start-button");
 const userScoreElement = document.querySelector("#user-score p");
 const computerScoreElement = document.querySelector("#computer-score p");
 const announcementTextElement = document.querySelector(".announcement-text h1");
+const userPickTextElement = document.querySelector(".user-pick h1");
 const selectionDiv = document.querySelector(".selection");
 let rockButton, paperButton, scissorsButton;
 
@@ -170,5 +229,5 @@ startButton.addEventListener("click", () => {
     computerScoreElement.textContent = computerScore;
 
     // Call the playGame function
-    playGame(5); // Play 5 rounds
+    playGame(1); // Best out of n
 });
